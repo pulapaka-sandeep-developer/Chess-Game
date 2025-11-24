a3f92aab197f373426ef140f2ecb29adbd6d8fc8
P Sandeep <122431442+pulapaka-sandeep-developer@users.noreply.github.com>
2025-11-24 17:44:37 +0530

    changes default routes chess

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,8 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { ProfileViewComponent } from './profile-view/profile-view.component';
-import { BasicInfoComponent } from './basic-info/basic-info.component';
 import { PlayZoneComponent } from './play-zone/play-zone.component';
 import { ChessComponent } from './chess/chess.component';
 
 export const routes: Routes = [
-  { path: '', component: PlayZoneComponent },
-  // { path: 'profile-view', component: ProfileViewComponent },
-  // { path: 'profile-info', component: BasicInfoComponent },
-  { path: 'game-chess', component: ChessComponent },
+  { path: '', redirectTo: 'chess', pathMatch: 'full' },
+  { path: 'chess', component: ChessComponent },
 ];
